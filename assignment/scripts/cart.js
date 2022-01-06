f15df7867ffadb72d03832d5faa22ce84a97dd23
Heather Kim <heatherkim0405@gmail.com>
2022-01-06 10:39:23 -0600

    tested functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,19 +1,38 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [];
+let basket = [ ];
+console.log(`basket is ${basket}`);
 
-function addItem('item'){
-  basket.push('item');
+function addItem(item){
+  basket.push(item);
   return true;
 };
 
-function listItmes(){
+console.log('Adding apples (expect true)', addItem('apples'));
+console.log(`Basket is now ${basket}`);
+
+console.log('Adding milk (expect true)', addItem('milk'));
+console.log(`Basket is now ${basket}`);
+
+console.log('Adding spinach (expect true)', addItem('spinach'));
+console.log(`Basket is now ${basket}`);
+
+
+
+function listItems(){
   for(let i = 0; i < basket.length; i++){
-    console.log('basket[i]'');
+    console.log(basket[i]);
   }
 };
+listItems();
+
 
 function empty(){
-  basket = [];
+  for(let i = basket.length-1; i >= 0; i--){
+    basket.pop(basket[i]);
+  }
+  return basket;
 };
+
+console.log('Emptying the basket, basket is now', empty());
