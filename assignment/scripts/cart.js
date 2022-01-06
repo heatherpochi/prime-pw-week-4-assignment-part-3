7219804890e1ed7221fbe488972a4fdd3ccd9a84
Heather Kim <heatherkim0405@gmail.com>
2022-01-06 11:51:22 -0600

    created an updated function for the stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -18,6 +18,11 @@ console.log(`Basket is now ${basket}`);
 console.log('Adding spinach (expect true)', addItem('spinach'));
 console.log(`Basket is now ${basket}`);
 
+console.log('Adding oranges (expect true)', addItem('oranges'));
+console.log(`Basket is now ${basket}`);
+
+console.log('Adding whole chicken (expect true)', addItem('whole chicken'));
+console.log(`Basket is now ${basket}`);
 
 
 function listItems(){
@@ -36,3 +41,51 @@ function empty(){
 };
 
 console.log('Emptying the basket, basket is now', empty());
+
+const maxItems = 5;
+
+function isFull(){
+  if(basket.length < 5){
+    return false;
+  }
+  return true;
+}
+
+console.log('Basket reached the maximum number of items', isFull());
+
+function updatedAddItem(item){
+  if(isFull() === true){
+    return false;
+  }
+
+  basket.push(item);
+  return true;
+};
+
+console.log('Adding apples (expect true)', updatedAddItem('apples'));
+console.log(`Basket is now ${basket}`);
+console.log('Basket reached the maximum number of items', isFull());
+
+console.log('Adding carrots (expect true)', updatedAddItem('carrots'));
+console.log(`Basket is now ${basket}`);
+console.log('Basket reached the maximum number of items', isFull());
+
+console.log('Adding oranges (expect true)', updatedAddItem('oranges'));
+console.log(`Basket is now ${basket}`);
+console.log('Basket reached the maximum number of items', isFull());
+
+console.log('Adding milk (expect true)', updatedAddItem('milk'));
+console.log(`Basket is now ${basket}`);
+console.log('Basket reached the maximum number of items', isFull());
+
+console.log('Adding spinach (expect true)', updatedAddItem('spinach'));
+console.log(`Basket is now ${basket}`);
+console.log('Basket reached the maximum number of items', isFull());
+
+console.log('Adding almond (expect true)', updatedAddItem('almond'));
+console.log(`Basket is now ${basket}`);
+console.log('Basket reached the maximum number of items', isFull());
+
+console.log('Adding salmon (expect true)', updatedAddItem('salmon'));
+console.log(`Basket is now ${basket}`);
+console.log('Basket reached the maximum number of items', isFull());
