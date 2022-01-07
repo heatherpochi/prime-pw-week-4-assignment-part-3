e3fdead0d3e54d87176ff0c35ab61db90fe44f30
Heather Kim <heatherkim0405@gmail.com>
2022-01-06 18:32:27 -0600

    revised console.logs

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,13 +1,13 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
-let basket = [ ];
-console.log(`basket is ${basket}`);
+let basket = [];
+console.log('basket is', basket);
 
 function addItem(item){
   basket.push(item);
   return true;
-};
+}
 
 console.log('Adding apples (expect true)', addItem('apples'));
 console.log(`Basket is now ${basket}`);
@@ -29,16 +29,15 @@ function listItems(){
   for(let i = 0; i < basket.length; i++){
     console.log(basket[i]);
   }
-};
+}
+
 listItems();
 
 
 function empty(){
-  for(let i = basket.length-1; i >= 0; i--){
-    basket.pop(basket[i]);
-  }
+  basket =[];
   return basket;
-};
+}
 
 console.log('Emptying the basket, basket is now', empty());
 
@@ -51,7 +50,7 @@ function isFull(){
   return true;
 }
 
-console.log('Basket reached the maximum number of items', isFull());
+console.log('Basket reached the maximum number of items (expect false)', isFull());
 
 function updatedAddItem(item){
   if(isFull() === true){
@@ -59,46 +58,45 @@ function updatedAddItem(item){
   }
   basket.push(item);
   return true;
-};
+}
 
 console.log('Adding apples (expect true)', updatedAddItem('apples'));
 console.log(`Basket is now ${basket}`);
-console.log('Basket reached the maximum number of items', isFull());
+console.log('Basket reached the maximum number of items (expect false)', isFull());
 
 console.log('Adding carrots (expect true)', updatedAddItem('carrots'));
 console.log(`Basket is now ${basket}`);
-console.log('Basket reached the maximum number of items', isFull());
+console.log('Basket reached the maximum number of items (expect false)', isFull());
 
 console.log('Adding oranges (expect true)', updatedAddItem('oranges'));
 console.log(`Basket is now ${basket}`);
-console.log('Basket reached the maximum number of items', isFull());
+console.log('Basket reached the maximum number of items (expect false)', isFull());
 
 console.log('Adding milk (expect true)', updatedAddItem('milk'));
 console.log(`Basket is now ${basket}`);
-console.log('Basket reached the maximum number of items', isFull());
+console.log('Basket reached the maximum number of items (expect false)', isFull());
 
 console.log('Adding spinach (expect true)', updatedAddItem('spinach'));
 console.log(`Basket is now ${basket}`);
-console.log('Basket reached the maximum number of items', isFull());
+console.log('Basket reached the maximum number of items (expect true)', isFull());
 
-console.log('Adding almond (expect true)', updatedAddItem('almond'));
+console.log('Adding almond (expect false)', updatedAddItem('almond'));
 console.log(`Basket is now ${basket}`);
-console.log('Basket reached the maximum number of items', isFull());
+console.log('Basket reached the maximum number of items (expect true)', isFull());
 
-console.log('Adding salmon (expect true)', updatedAddItem('salmon'));
+console.log('Adding salmon (expect false)', updatedAddItem('salmon'));
 console.log(`Basket is now ${basket}`);
-console.log('Basket reached the maximum number of items', isFull());
+console.log('Basket reached the maximum number of items (expect true)', isFull());
 
 function removeItem(item){
-  let removedItem = [];
   if(basket.includes(item) === false){
     return null;
   }
   basket.indexOf(item);
-  removedItem = basket.splice(basket.indexOf(item), 1);
+  let removedItem = basket.splice(basket.indexOf(item), 1);
   return removedItem;
 }
 
-console.log(removeItem('sugar'));
-console.log(removeItem('milk'));
+console.log('Removing sugar (expect null)',removeItem('sugar'));
+console.log('Removing milk, the removed item is:',removeItem('milk'));
 console.log(basket);
